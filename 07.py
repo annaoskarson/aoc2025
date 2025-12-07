ece484fbf1cf96b888c31e61b0f903b6405bb79f
Anna <anna.oskarson@gmail.com>
2025-12-07 11:44:49 +0100

    Added part 1 into part 2.

--- 07.py
+++ 07.py
@@ -9,30 +9,10 @@ for r,row in enumerate(lines):
         elif ch == 'S':
             s = (r,c)
 
-def down(beams, rn, splitters):
-    splits = 0 # Number of splits.
-    newbeams = set() # Make a new set for the next row.
-    for beam in beams:
-        if (rn, beam) in splitters:
-            splits += 1
-            newbeams.add(beam-1)
-            newbeams.add(beam+1)
-        else:
-            newbeams.add(beam)
-    return(newbeams, splits)
-
 splits = 0
-beams = set()
-beams.add(s[1]) # The beams om this row.
-
-for r,line in enumerate(lines):
-    beams, newsplits = down(beams, r, splitters)
-    splits += newsplits
-    
-print('Advent of code, day 7, part 1:', splits)
-
 brainz = {} # We need a brainz dictionary remember things.
 def trickle(p):
+    global splits
     r,c = p # This is where we are
 
     if r >= len(lines): # At the bottom
@@ -43,11 +23,12 @@ def trickle(p):
 
     if (r,c) in splitters: # Split! And we only need to check every other row. :-)
         brainz[(r,c)] = trickle((r+2, c-1)) + trickle((r+2, c+1))
+        splits += 1
     
     else: # No split, continue. And we only need to check every other row. :-)
         brainz[(r,c)] = trickle((r+2, c))
     return(brainz[(r,c)])
 
 ans2 = trickle(s)
-
+print('Advent of code, day 7, part 1:', splits)
 print('Advent of code, day 7, part 2:', ans2)
